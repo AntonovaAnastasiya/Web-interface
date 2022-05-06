10e7b4d38501cc203da0d26669da8574697d93d7
PS <89146019257@mail.ru>
2022-05-06 21:55:01 +0900

    add Happy path tests

---
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -0,0 +1,36 @@
+package ru.netology.web;
+
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class CardOrderTest {
+
+    @Test
+    public void testHappyPath() {
+
+        open("http://localhost:9999");
+        $(".heading heading_size_l heading_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Анастасия Антонова");
+        $("[data-test-id=phone] input").setValue("+79146562545");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    public void testHappyPath2() {
+
+        open("http://localhost:9999");
+        $(".heading heading_size_l heading_theme_alfa-on-white");
+        $("[data-test-id=name] input").setValue("Анастасия-Виктория Петровна");
+        $("[data-test-id=phone] input").setValue("+70000000002");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
