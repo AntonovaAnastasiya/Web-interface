b7903a3bf41392524b403b7772322bab5056aa02
PS <89146019257@mail.ru>
2022-05-08 18:28:28 +0900

    Change selenude on selenium

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,46 +1,71 @@
 package ru.netology.web;
 
-import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.List;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class CardOrderTest {
+class CardOrderTest {
+    private WebDriver driver;
 
     @BeforeAll
-    public static void setUpAll(){
+    public static void setUp() {
         WebDriverManager.chromedriver().setup();
     }
 
-    @Test
-    public void testHappyPath() {
-        Configuration.holdBrowserOpen = true;
 
-        open("http://localhost:9999");
-        $(".heading heading_size_l heading_theme_alfa-on-white");
-        $("[data-test-id=name] input").setValue("Анастасия Антонова");
-        $("[data-test-id=phone] input").setValue("+79146562545");
-        $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    @BeforeEach
+    public void setUp1() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @BeforeEach
+    void setUp2() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void close() {
+        driver.quit();
+        driver = null;
     }
 
     @Test
-    public void testHappyPath2() {
-        Configuration.holdBrowserOpen = true;
-
-        open("http://localhost:9999");
-        $(".heading heading_size_l heading_theme_alfa-on-white");
-        $("[data-test-id=name] input").setValue("Анастасия-Виктория Петровна");
-        $("[data-test-id=phone] input").setValue("+70000000002");
-        $("[data-test-id=agreement]").click();
-        $("[type=button]").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    void shouldTestV1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Анастасия Антонова");
+        elements.get(1).sendKeys("+79146562545");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
+    @Test
+    void shouldTestV2() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Анастасия-Виктория Петровна");
+        elements.get(1).sendKeys("+70000000002");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
 }
+
