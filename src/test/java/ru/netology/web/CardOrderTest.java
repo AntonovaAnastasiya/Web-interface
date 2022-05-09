4f60f9a1ab5df4bf1c9b9e0bfa9c2fe811582b55
PS <89146019257@mail.ru>
2022-05-09 21:18:54 +0900

    Add tests which should check validation

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,18 +1,18 @@
 package ru.netology.web;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class CardOrderTest {
     WebDriver driver;
@@ -76,5 +76,38 @@ class CardOrderTest {
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
+
+    @Test
+    void shouldCheckValidationName() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анастасия-ВиктоRQрия Петровна");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+70000000002");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+
+    @Test
+    void shouldCheckValidationPhone() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анастасия-Виктория Петровна");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+70000гшнгнк0002");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+    }
+
+//    @Test
+//    void shouldCheckValidationCheckboxText() {
+//        driver.get("http://localhost:9999");
+//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анастасия-Виктория Петровна");
+//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+70000000002");
+//        driver.findElement(By.className("button")).click();
+//        String text = driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getText();
+//        Assertions.assertTrue(driver.findElement(By.cssSelector(".input_invalid .checkbox__text")).getCssValue("color").equals("#ff5c5c!important"));
+//    }
 }
+
 
